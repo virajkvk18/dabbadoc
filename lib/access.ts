3642c72f6c9a082f8f304cfc9c5abc1095017c7a
Viraj Kumar Vishwakarma <virajvishwakarma672@gmail.com>
2026-06-18 13:15:31 +0530

    Add friend to premium subscription

--- lib/access.ts
+++ lib/access.ts
@@ -4,7 +4,8 @@ import { isPaidPlan } from "@/lib/plans";
 const CREATOR_EMAILS = new Set([
   "aanya1407.jain@gmail.com",
   "jainaanya1407@gmail.com",
-  "virajvishwakarma672@gmail.com"
+  "virajvishwakarma672@gmail.com",
+  "yashjain18092005@gmail.com"
 ]);
 
 export function isCreatorEmail(email?: string | null) {
